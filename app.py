4e044b8d8365ae9cc7a7443abeffda7de338ed93
tpanthera <bisht11390@gmail.com>
2018-09-06 07:44:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+# simple rest api based on falcon
+
+import json,falcon
+
+class obRequestClass:
+    def on_get(self,req,resp):
+        content = {
+            'name' : 'Manish' ,
+            'place' : 'Bengaluru'
+        }
+        resp.body = json.dumps(content)
+
+api = falcon.API()
+api_add_route('/test',objRequestClass())
+
+
+'''
+Run in command line
+gunicorn app:api
+
+
+'''
