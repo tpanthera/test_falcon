e22e45c7fa5ca03f83b6ecd8f0d77308de58576a
tpanthera <bisht11390@gmail.com>
2018-09-11 09:50:33 +0530

    Create assign2_test.py

---
+++ assign2_test.py
@@ -0,0 +1,49 @@
+import falcon 
+import pytest
+
+class tyre:
+	def __init__():
+		self.width =
+
+	#setter 
+	#getter 
+
+	def on_post(self, req, resp):
+		if(req.path == two_wheel):
+			two_wheel_call = two_wheel()
+
+		if(req.path == four_wheel):
+			four_wheel_call = four_wheel()
+
+		if(req.path == six_wheel):
+			six_wheel_call = six_wheel()
+
+class two_wheel:
+
+class four_wheel:
+
+class six_wheel:
+
+application = falcon.API()
+application.add_route(/{wheel_type}, Tyre())
+
+# python test framework 
+
+# create main class , call that , which inturn will call api class and get the assert thing 
+
+#You must create an `app` fixture to expose the Falcon application you want to test:
+
+@pytest.fixture
+def app():
+return application
+
+# test_ to discover the method to be tested 
+
+## Fixtures ### client
+
+def test_post(client):
+resp = client.post('/route', {'myparam': 'myvalue'})
+assert resp.status == falcon.HTTP_OK
+assert resp.json['myparam'] == 'myvalue'
+
+
